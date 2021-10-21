68dd8074e8e2a7f0f1ffaa3d5f7b290be1768781
Jonathan Cruz <profesorjonathancruz@gmail.com>
2021-10-21 15:29:07 -0300

    + Sistema de Solicitudes.

---
+++ sistemaSolicitudes/Modelo.sql
@@ -0,0 +1,34 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS `Usuarios`;
+DROP TABLE IF EXISTS `Solicitudes`;
+DROP TABLE IF EXISTS `SolicitudesEstados`;
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE `Usuarios` (
+    `UsuarioId` INTEGER NOT NULL AUTO_INCREMENT,
+    `Nombre` VARCHAR(64) NOT NULL,
+    `Email` VARCHAR(128) NOT NULL,
+    `Password` VARCHAR(128) NOT NULL,
+    `Borrado` DATETIME NULL,
+    PRIMARY KEY (`UsuarioId`)
+);
+
+CREATE TABLE `Solicitudes` (
+    `SolicitudId` INTEGER NOT NULL AUTO_INCREMENT,
+    `Fecha` DATE NOT NULL,
+    `Detalles` VARCHAR(1024) NOT NULL,
+    `UsuarioId` INTEGER NOT NULL,
+    `EstadoId` INTEGER NOT NULL,
+    `Borrado` DATETIME,
+    PRIMARY KEY (`SolicitudId`)
+);
+
+CREATE TABLE `SolicitudesEstados` (
+    `EstadoId` INTEGER NOT NULL AUTO_INCREMENT,
+    `Descripcion` VARCHAR(64) NOT NULL,
+    `Borrado` DATETIME,
+    PRIMARY KEY (`EstadoId`)
+);
+
+ALTER TABLE `Solicitudes` ADD FOREIGN KEY (`UsuarioId`) REFERENCES `Usuarios`(`UsuarioId`);
+ALTER TABLE `Solicitudes` ADD FOREIGN KEY (`EstadoId`) REFERENCES `SolicitudesEstados`(`EstadoId`);
